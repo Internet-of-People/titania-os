9456294567f8a9ae1eef19b612427e701cd9c8bd
Pooja <pooja.maheshwari247@gmail.com>
2017-10-09 01:20:21 +0530

    resting page

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome aboard, Titania'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  font-weight: normal;
+}
+</style>

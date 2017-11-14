aa4b5e843abd9d26371e6c59a13019d734cdf64b
Oleksiy Protas <elfy.ua@gmail.com>
2017-11-14 13:33:23 +0200

    systemd unit for vuedj

---
+++ meta-titania/recipes-titania/vuedj/files/start_vuedj.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python monit_init.py
+python monit_dashboard.py
+python manage.py makemigrations
+python manage.py makemigrations configtitania
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
+

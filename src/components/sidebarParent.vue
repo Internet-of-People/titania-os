5e5a40db5a8be43008feef590f6eb8016b720cf7
Pooja <pooja.maheshwari247@gmail.com>
2017-12-01 00:55:47 +0530

    dashboard and sidebar complete

--- src/components/sidebarParent.vue
+++ src/components/sidebarParent.vue
@@ -6,14 +6,17 @@
     <div @click="tabSwitch('')" title='Dashboard'>
       <img class="logo-icon-small cursor-pointer" src="../assets/images/titania-logo-clean.svg">
     </div>
-    <div class='margin-top-20 cursor-pointer' title='dApps Console' @click="tabSwitch('dappsconsole')" v-bind:class="{ activeDrilldown: currentTab('dappsconsole')}">
-      <img class="tab-label" src="../assets/images/dApps-icon.svg">
+    <div class='margin-top-20 cursor-pointer' title='dApps Console' @click="tabSwitch('dappsconsole')" >
+      <img v-if="currentTab('dappsconsole')" class="tab-label" src="../assets/images/dApps-selected.svg">
+      <img v-else class="tab-label" src="../assets/images/dApps-icon.svg">
     </div>
-    <div @click="tabSwitch('stats')" title='dApps Stats' class='cursor-pointer' v-bind:class="{ activeDrilldown: currentTab('stats')}">
-      <img class="tab-label" src="../assets/images/uptime-icon.svg">
+    <div @click="tabSwitch('stats')" title='dApps Stats' class='cursor-pointer'>
+      <img v-if="currentTab('stats')" class="tab-label" src="../assets/images/uptime-selected.svg">
+      <img v-else class="tab-label" src="../assets/images/uptime-icon.svg">
     </div>
-    <div @click="tabSwitch('threads')" title='Threads' class='cursor-pointer' v-bind:class="{ activeDrilldown: currentTab('threads')}">
-      <img class="tab-label" src="../assets/images/thread-icon.svg">
+    <div @click="tabSwitch('threads')" title='Threads' class='cursor-pointer'>
+      <img v-if="currentTab('threads')" class="tab-label" src="../assets/images/thread-selected.svg">
+      <img v-else class="tab-label" src="../assets/images/thread-icon.svg">
     </div>
   </div>
 </template>

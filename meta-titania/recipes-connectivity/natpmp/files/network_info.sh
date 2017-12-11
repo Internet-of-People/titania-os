7e28e58eb666452a40c9aa29df514b61dadb4592
Oleksiy Protas <elfy@ecognize.me>
2017-12-11 21:16:42 +0200

    Support of being directly connected to the Internet

--- meta-titania/recipes-connectivity/natpmp/files/network_info.sh
+++ meta-titania/recipes-connectivity/natpmp/files/network_info.sh
@@ -4,27 +4,32 @@
 # TODO: compile grep with PRCE and use lookaround instead of two greps
 # sed is ugly
 
-ROUTER_IP=$(natpmpc | grep -o 'Public IP.*$' | grep -o '[0-9.]*')
 IPINFO=$(curl -s https://ipinfo.io)
-EXTERNAL_IP=$(echo $IPINFO | grep -o '"ip": "[0-9.]*"' | grep -o '[0-9.]*')
 LOCATION=$(echo $IPINFO | grep -o '"loc": "[0-9,.]*"' | grep -o '[0-9,.]*')
+echo -e "Location:\t\t\t$LOCATION"
 
-echo -e "Router reported address:\t$ROUTER_IP"
+EXTERNAL_IP=$(echo $IPINFO | grep -o '"ip": "[0-9.]*"' | grep -o '[0-9.]*')
 echo -e "Address seen from outside:\t$EXTERNAL_IP"
 
-if [ "$ROUTER_IP" != "$EXTERNAL_IP" ] ; then
-    echo "Router's declared IP does not match the public one"
-    echo "Possibly the router is behind NAT, aborting operation"
-    exit -1
-fi
+if /sbin/ifconfig | grep -vq "addr:${EXTERNAL_IP}" ; then
+    echo "We seem to be behind the router, trying NAT-PMP"
 
-echo -e "Location:\t\t\t$LOCATION"
+    ROUTER_IP=$(natpmpc | grep -o 'Public IP.*$' | grep -o '[0-9.]*')
+    echo -e "Router reported address:\t$ROUTER_IP"
+
+    if [ "$ROUTER_IP" != "$EXTERNAL_IP" ] ; then
+        echo "Router's declared IP does not match the public one"
+        echo "Possibly the router is behind NAT, aborting operation"
+        exit -1
+    fi
+else
+    echo "We seem to be directly connected to the internet"
+fi
 
 LATITUDE=$(echo $LOCATION | grep -o '^[0-9.]*')
 LONGITUDE=$(echo $LOCATION | grep -o '[0-9.]*$')
 
-# Should be the same as EXTERNAL_IP, right?
 # TODO: race conditions when run in parallel a few times, do a HEREDOC or something
-echo "PUBLIC_IP='$ROUTER_IP'" > /run/network_info.env
+echo "PUBLIC_IP='$EXTERNAL_IP'" > /run/network_info.env
 echo "LATITUDE='$LATITUDE'" >> /run/network_info.env
 echo "LOGNITUDE='$LONGITUDE'" >> /run/network_info.env

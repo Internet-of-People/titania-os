85907b7bc4d9b1e529a3fba2ab3142f484529951
Oleksiy Protas <elfy@ecognize.me>
2018-03-17 01:12:21 +0100

    Switching logic moved away from u-boot
    Makes double updates predictable

--- meta-titania/recipes-support/swupdate/files/update_system.sh
+++ meta-titania/recipes-support/swupdate/files/update_system.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Double update
+if fw_printenv after_update; then
+    echo "Update flag is on, deactivating it preventively"
+    fw_setenv after_update
+fi
+
 # TODO: usage etc
 ACTIVE_ROOT=$(fw_printenv -n active_root)
 if test $? -ne 0 ; then
@@ -10,3 +16,6 @@ fi
 echo "Currently active root: ${ACTIVE_ROOT}, flashing the other one."
 
 swupdate -v -e stable,${ACTIVE_ROOT} -i $*
+
+# TODO: make it a part of swupdate
+rm -f /tmp/swupdateprog

deac3d8f9325dccb3d235f769fc3e53f57122fdf
Oleksiy Protas <elfy.ua@gmail.com>
2018-06-08 21:50:50 +0300

    Manual latest tag upkeeping

--- meta-titania/recipes-titania/dapp/files/dapp_load.sh
+++ meta-titania/recipes-titania/dapp/files/dapp_load.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # If there is a file, import
 # TODO: threat model assumes attackers won't be able to write there unless they are root
-if test -f "$1"; then
+DOCKER_IMAGE_FILE=/var/lib/docker/preinstall/$(echo $1 | tr '/:@' '___').tar
+
+if test -f "$DOCKER_IMAGE_FILE"; then
     echo "Loading dApp image"
-    docker load -i "$1" && rm -f "$1"
+    docker load -i "$DOCKER_IMAGE_FILE" && \
+        rm -f "$DOCKER_IMAGE_FILE" && \
+        docker tag $1 $(grep -o '^[^@]*'<<<$1):latest
 else
     echo "dApp pre-downloaded image not found"
 fi
--- meta-titania/recipes-titania/dapp/files/dapp_pull.sh
+++ meta-titania/recipes-titania/dapp/files/dapp_pull.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # If we still don't have an image, pull it
-if test -z "$(docker images -q $1)"; then
+DOCKER_IMAGE_BASE=$(grep -o '^[^@]*'<<<$1)
+
+if test -z "$(docker images -q $DOCKER_IMAGE_BASE)"; then
     echo "Downloading dApp image"
-    docker pull $1
+    docker pull $1 && docker tag $1 $DOCKER_IMAGE_BASE:latest
 else
     echo "dApp image already downloaded"
 fi
